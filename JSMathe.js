a848d32feed39866f7ab897dbd7601c63dd4b25a
SAnnaS4 <aquary1998@web.de>
2017-06-27 16:03:22 +0200

    buttons
    design überarbeitung (über articel lag ein element-->war vom user nciht
    nutzbar!)

---
+++ JSMathe.js
@@ -0,0 +1,8 @@
+function myFunction(elmt1,elmt2) {
+	  document.getElementById(elmt1).style.zIndex="-2";
+	  document.getElementById(elmt2).style.zIndex="2";
+}
+function myFunctionZ(elmt2,elmt1) {
+	  document.getElementById(elmt1).style.zIndex="-2";
+	  document.getElementById(elmt2).style.zIndex="2";
+}
